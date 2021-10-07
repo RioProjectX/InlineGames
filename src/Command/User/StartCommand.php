c6bc6772e952deefaf10799edd4cd2a195f6f9b8
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-10-07 09:06:09 +0700

    Update StartCommand.php

--- src/Command/User/StartCommand.php
+++ src/Command/User/StartCommand.php
@@ -57,8 +57,8 @@ class StartCommand extends UserCommand
         }
 
         $text = Emoji::wavingHand() . ' ';
-        $text .= '<b>' . __('Hi!') . '</b>' . PHP_EOL;
-        $text .= __('To begin, start a message with {USAGE} in any of your chats or click the {BUTTON} button and then select a chat to play in.', ['{USAGE}' => '<b>\'@' . $this->getTelegram()->getBotUsername() . ' ...\'</b>', '{BUTTON}' => '<b>\'' . __('Play') . '\'</b>']);
+        $text .= '<b>' . __('Halo! Saya InlinesGame Bot. Made By @fckualot') . '</b>' . PHP_EOL;
+        $text .= __('Untuk Bermain, Mulai Sebuah Pesan Dengan {USAGE} Didalam Group Chat Kamu Atau Klik Tombol {BUTTON} dan berikutnya pilih group chat untuk bermain.', ['{USAGE}' => '<b>\'@' . $this->getTelegram()->getBotUsername() . ' ...\'</b>', '{BUTTON}' => '<b>\'' . __('Play') . '\'</b>']);
 
         $data = [
             'chat_id'                  => $chat_id,
